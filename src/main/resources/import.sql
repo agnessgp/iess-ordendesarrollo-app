2da47d6508ea755aab5f2792cff19de012b20204
agnessgp <agnessgp@gmail.com>
2018-10-22 16:49:06 -0500

    Insert Base de datos por defecto

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO des_modulo_t (id, nombre, componente) VALUES (1,'LOGIN DE USUARIO','PLANTILLA');
-INSERT INTO des_modulo_t (id, nombre, componente) VALUES (1,'PLANTILLA FRONT','PLANTILLA');
-INSERT INTO des_catalogodetalle_t (codigo_catalogo,codigo_detalle,descripcion,valor,orden,estado) VALUES ('TIPDESA','DES1','NUEVO REQUERIMIENTO','1',1,'ACT');
-INSERT INTO des_catalogodetalle_t (codigo_catalogo,codigo_detalle,descripcion,valor,orden,estado) VALUES ('TIPDESA','DES2','CAMBIO DE FUNCIONALIDAD','2',2,'ACT');
-INSERT INTO des_catalogodetalle_t (codigo_catalogo,codigo_detalle,descripcion,valor,orden,estado) VALUES ('TIPDESA','DES3','DESARROLLO INCOMPLETO','3',3,'ACT');
+--INSERT INTO des_modulo_t (id, nombre, componente) VALUES (1,'LOGIN DE USUARIO','PLANTILLA');
+--INSERT INTO des_modulo_t (id, nombre, componente) VALUES (1,'PLANTILLA FRONT','PLANTILLA');
+--INSERT INTO des_catalogodetalle_t (codigo_catalogo,codigo_detalle,descripcion,valor,orden,estado) VALUES ('TIPDESA','DES1','NUEVO REQUERIMIENTO','1',1,'ACT');
+--INSERT INTO des_catalogodetalle_t (codigo_catalogo,codigo_detalle,descripcion,valor,orden,estado) VALUES ('TIPDESA','DES2','CAMBIO DE FUNCIONALIDAD','2',2,'ACT');
+--INSERT INTO des_catalogodetalle_t (codigo_catalogo,codigo_detalle,descripcion,valor,orden,estado) VALUES ('TIPDESA','DES3','DESARROLLO INCOMPLETO','3',3,'ACT');
 
